8ddc7c3a239bba74ac478f3b6ccea89b1304660f
GRU996 <86210258+GRU996@users.noreply.github.com>
2022-12-31 16:52:47 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,90 +1,72 @@
 #!/bin/bash
 
-Config
 name='Les deseo un feliz año nuevo - por GRU996'
-correction=0
-new_year=$(( $(date +'%Y') + 1))
-
-Trap to reset terminal and exit on Ctrl-C
 trap "tput reset; tput cnorm; exit" 2
 
-Hide cursor
+
 tput civis
 
-Initialize variables
 line=6
-middle_column=$(( $(tput cols) / 3 ))
-middle_column=$(( middle_column - 0 ))
+middle_column=$(($(tput cols) / 3))
+middle_column=$((middle_column-0))
 color=0
 
-Loop forever
+new_year=$(date +'%Y')
+let new_year++
+
 while true; do
+    randomValue=$((RANDOM % 12 + 12)) ##
+    from_middle=$((middle_column-randomValue))
+    column=$((RANDOM % (randomValue) * 2 + 1 + from_middle))
+    tput setaf $color; tput bold
 
-Generate random values
-random_value=$(( RANDOM % 12 + 12 ))
-from_middle=$(( middle_column - random_value ))
-column=$(( RANDOM % (random_value) * 2 + 1 + from_middle ))
+    tput cup 6 $column
+    echo '#'
 
-Set text color and bold
-tput setaf $color; tput bold
+    color=$(((color+7)%8))
 
-Display '#' at random column
-tput cup 6 $column
-echo '#'
+    from_middle=-8 # from left column
+    for l in F E L I Z
+    do
+        tput cup $((line+1)) $((middle_column+from_middle))
+        echo $l
+        let from_middle++
+        sleep 0
+    done
 
-Increment color value
-color=$(( (color + 7) % 8 ))
+    from_middle=0
+    for l in A Ñ O
+    do
+        tput cup $((line+2)) $((middle_column+from_middle))
+        echo $l
+        let from_middle++
+        sleep 0
+    done
 
-Display "HAPPY" at the top of the screen
-from_middle=-8 # from left column
-for l in H A P P Y
-do
-tput cup $(( line + 1 )) $(( middle_column + from_middle ))
-echo $l
-from_middle=$(( from_middle + 1 ))
-sleep 0
-done
+    from_middle=8
+    for l in N U E V O
+    do
+        tput cup $((line+3)) $((middle_column+from_middle))
+        echo $l
+        let from_middle++
+        sleep 0
+    done
 
-Display "NEW" below "HAPPY"
-from_middle=0
-for l in N E W
-do
-tput cup $(( line + 2 )) $(( middle_column + from_middle ))
-echo $l
-from_middle=$(( from_middle + 1 ))
-sleep 0
-done
+    spaceCount=$((middle_column/9 + correction))
+    spaces=""
+    for (( c=1; c<=$spaceCount; c++ ))
+    do
+        spaces+=" "
+    done
 
-Display "YEAR" below "NEW"
-from_middle=8
-for l in Y E A R
-do
-tput cup $(( line + 3 )) $(( middle_column + from_middle ))
-echo $l
-from_middle=$(( from_middle + 1 ))
-sleep 0
-done
+    tput cup $((line+5)) 1
+    toilet "${spaces}$new_year" --metal -t
 
-Calculate number of spaces to center the new year
-space_count=$(( middle_column / 9 + correction ))
-spaces=""
-for (( c=1; c<=$space_count; c++ ))
-do
-spaces+=" "
-done
+    tput setaf $color; tput bold
 
-Display new year using toilet command
-tput cup $(( line + 5 )) 1
-toilet "${spaces}$new_year" --metal -t
+    tput cup 10 $column
+    echo '#'
 
-Set text color and bold
-tput setaf $color; tput bold
-
-Display '#' at random column
-tput cup 10 $column
-echo '#'
-
-Display name at the bottom of the screen
-tput cup $(( line + 12 )) $(( middle_column - ( ${#name} / 2 ) ))
-echo $name
+    tput cup $((line+12)) $((middle_column-(${#name}/2)))
+    echo $name
 done
